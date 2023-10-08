5beff8ba2e58a42d071ab934915fa64f65e358ad
nerovkha <144420486+nerovkha@users.noreply.github.com>
2023-10-08 12:40:03 +0300

    Multiplication
    here i do multiplication

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,30 @@ public class Main {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
 
-System.out.println("The");
+String creator="Nerov kha";
+
+System.out.println("This app is mage by: " + creator );
+
+int number1=5;
+
+
+//System.outt.println(number1);
+
+int number2=3;
+System.out.println("number2");
+
+int multiplication = number1*number2;
+System.out.println(multiplication);
+
+
+
+
+
+
+
+
+
+
 
 
 	}
